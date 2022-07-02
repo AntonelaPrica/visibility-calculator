fd1b022f0d4f2b3cf24e488526fefae6e2b75c3b
Antonela <pricaantonela@yahoo.com>
2022-07-02 21:25:03 +0300

    feat: upgraded to nx 14

---
+++ apps/visibility-calculator-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'ro-ubb-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+	title = 'visibility-calculator-ui';
+}
